9df7000a3bbc377d738e862c039abec574979ae5
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-05-19 00:37:22 -0500

    Create prac.java

---
+++ prac.java
@@ -0,0 +1,17 @@
+/*
+    Messing with subStrings
+*/
+
+
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+         String word = "Barefoot";
+         // index       01234567
+         String sub7 = word.substring(1, 4); //are
+         String sub8 = word.substring(2,3) + word.substring(5, 7) + word.substring(4, 5); // roof
+        System.out.println(sub7);
+        System.out.println(sub8);
+     }
+}
