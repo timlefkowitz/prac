e09908927b0e5191597e22aca58eeaf8d73f79b5
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-06-12 19:47:28 -0500

    Update appendToSQLscript.java

--- appendToSQLscript.java
+++ appendToSQLscript.java
@@ -4,32 +4,56 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Arrays;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static java.lang.Integer.parseInt;
 
+/**
+ *
+ */
 
 
-    public class append {
-        public static void write(String s, File f) throws IOException{
-            FileWriter fw = new FileWriter(f, true);
-            fw.write(s);
-            fw.close();
-        }
+public class append {
+    
+    public static void main(String[] args){
+        int count=0;
+        try{
 
+            File f = new File("C:\\Projects\\c.sql");
+            FileWriter fw = new FileWriter(f);
+            String sql= "(Insert into CARBS(HAM,BURGER,TEA) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+//            String filepath = "C:\\v.csv";
+            String filepath = "C:\\Projects\\Sheet.csv";
 
-        public static void main(String[] args){
-
-            String filepath = "filepathtoCSV";
-            int count=0;
-
-
-            lineReader.readLine();
-
-            try{
-                File f = new File("filepathToAppend.sql");
-                String sql="Insert into TABLE(LABEL,PRICE_TYPE,SKU,ZONE,min,max,) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            try {
                 BufferedReader lineReader=new BufferedReader(new FileReader(filepath));
-                write(sql);
+                String line;
+                while((line = lineReader.readLine()) != null){
+                    String[] items = line.split(",");
+                    String itemsQ = Stream.of(items).collect(Collectors.joining("\",\"", "\"" , "\""));
+                    System.out.println(itemsQ);
+
+                    fw.write(  "\n"+"Insert into CARBS (HAM,BURGER,TEA) values (''" + itemsQ);
+
+                }
+//                String inputLine = lineReader.readLine();
+//               fw.write(inputLine);
+               fw.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-            catch(IOException e)
+
         }
+        catch(IOException e){
+            e.printStackTrace();
+
+        }
+
     }
+
+
+}
