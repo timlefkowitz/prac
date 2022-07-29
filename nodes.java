840819b211bdd99bc2dc1ee66a0a21af0fd7ad2f
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-07-28 22:46:48 -0500

    nodes

--- nodes.java
+++ nodes.java
@@ -31,12 +31,78 @@ static class BinarySearchTree {
                 } else if(node.left !== null){
                     return searchTree(node.left);
                 }
-            } else if(node.right !== null){
-                return searchTree(node.right);
-            } 
-        }
-     }
+            } else if(data > node.data){
+                if(node.right === null){
+                    node.right = new Node(data);
+                    return;
+                } else if(node.right !== null){
+                    return searchTree(node.right);
+                }
+            } else {
+                return null;
+            }
+        } ;
+        return searchTree(node);
+     } 
+ }
 
+ findMin(){
+    let current = this.root;
+    while(current.left !== null){
+        current = current.left;
+    }
+    return current.data;
+ }
+
+ findMax(){
+    let current = this.root;
+    while(current.right !== null){
+        current = current.right;
+    }
+    return current.data;
+ }
+
+ find(){
+    let current = this.root;
+    while(current.data !== data){
+        if(data < current.data){
+            current = current.left;
+        } else {
+            current = current.right;
+        }
+        if(current === null){
+            return null;
+        }
+    }
+    return current;
+ }
+
+ isPresent(data){
+    let current = this.root;
+    while(current){
+        if(data === current.data){
+            return true;
+        }
+        if(data < current.data) {
+            current = current.left;
+        } else {
+            current = current.right;
+        }
+    }
+    return false;
+ }
+ remove(data){
+    const removeNode = function(node,data){
+        if(node == null){
+            return null;
+        }
+        if(data == node.data) {
+            // node has no childern
+            if(node.left == null && node.right == null){
+                return null;
+            }
+        }
+    }
  }
 
 
