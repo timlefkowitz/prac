4190f103f9de2e6d73cea4f10340345d442c1b7a
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-07-28 22:10:24 -0500

    nodes

--- nodes.java
+++ nodes.java
@@ -2,12 +2,27 @@ static class BinarySearchTree {
     Node root;
     
     Node findInOrderSuccessor(Node inputNode){
-
         void insert(int key){
             if(this.root == null){
                 this.root = new Node(key);
-                return;
+                return parent;
             }
         }
+    }
+
+    node getMostLeft(node x) {
+        x.left == null){
+            return x;
+        }
+    }
+
+
+ }
+
+ static class Node {
+    constructor(data, left = null, right = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
     }
  }
