9a29962c60c5a24a30b0efe4ce8e7d2980592216
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-07-29 22:29:32 -0500

    nodes

--- olla-programmer.js
+++ olla-programmer.js
@@ -17,7 +17,12 @@ const depthFirstPrint = (graph, source) => {
     const breadthFirstPrint = (graph, source) => {
         const queue = [source ];
         while(queue.length > 0){
-            
+            const current = quese.shift();
+            console.log(current);
+            for(let neighbor of graph[current]) }{
+                queue.push(neighbor);
+            }
+
         }
     };
 
