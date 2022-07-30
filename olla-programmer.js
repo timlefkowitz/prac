d061ba0de7e52749890d55830f599032943e0f97
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-07-29 19:27:06 -0500

    graphs

--- olla-programmer.js
+++ olla-programmer.js
@@ -1,1 +1,18 @@
+const depthFirstPrint = (graph, source) => {
+    const stack = [source];
 
+    while(stack = null){
+
+    };
+
+    const graph = {
+        a: ['6','c'],
+        b: ['d'],
+        c: ['e'],
+        d: ['f'],
+        e: [],
+        f: []
+    }
+
+    depthFirstPrint(graph, 'a');
+}
