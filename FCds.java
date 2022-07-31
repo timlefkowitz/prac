2954d089e5f3bee2b19d544397884bcc66e0ca52
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-07-30 20:31:40 -0500

    bigo

--- FCds.java
+++ FCds.java
@@ -14,4 +14,16 @@
         Exponential Time: 0(bn), b > 1
         Factorial Time: 0(n!)
 
+        Big-0 Properties
+
+        0(n+c) = 0(n)
+        0(cn) = 0(n), c > 0
+
+        let f be a function that describes the running time
+        of a particular algorithm for an input of size n :
+
+        f(n) = 7log(n)3 + 15n2 + 2n3 + 8
+
+            0(f(n)) = 0(n3)
+
  */
