160ba4b7e5006a3cb4a699d2a3482ce17c2cc8b8
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-07-30 20:27:46 -0500

    bigO

---
+++ FCds.java
@@ -0,0 +1,17 @@
+
+
+/**
+
+        Big-0 Notation 
+        n - The size of the input 
+        Complexities ordered in from smallest to largest 
+
+        Contant Time: 0(1)
+        Logarithmic Time: 0(log(n))
+        Linear Time: 0(n)
+        Linearithmic Time: 0(n2)
+        Cubic Tme: 0(n3)
+        Exponential Time: 0(bn), b > 1
+        Factorial Time: 0(n!)
+
+ */
