8d20e148f3ece1df86368c189eea7e11009912bb
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-11-07 11:08:37 -0600

    convert temp

--- 100JavaExercises.java
+++ 100JavaExercises.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
+        // Even or Odd
+
         System.out.println("Enter a number: ");
         Scanner sc = new Scanner(System.in);
         int number = Integer.parseInt(sc.nextLine());
@@ -15,5 +17,18 @@ class Main {
         else {
             System.out.println("The number is Odd");
         }
+
+        // Convert temperature in Degrees
+
+        System.out.println("Enter temperature in Centigrade: ");
+        Scanner sc = new Scanner(System.in);
+        int c = Integer.parseInt(sc.nextLine());
+        float f = ((9*c)/5)+32;
+
+        System.out.println("Temperature in Fahrenheit is: "+f);
+
+
+
+
     }
 }
