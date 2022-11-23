3ede562f7dccdc9517ee6562abadd6bedd97dc17
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2022-11-23 00:50:42 -0600

    dayOne

--- adventOfCode.java
+++ adventOfCode.java
@@ -1,3 +1,30 @@
 public static adventOfCode {
-    
+    public static void main(String[] args) throws FileNotFoundException {
+
+    try {
+
+        BufferedReader br = new BufferedReader(new FileReader("inputs/inputsDayOne.txt"));
+        int countIncrease = 0;
+        int lastNum = -1;
+        String line;
+        while((line = br.readLine()) != null) {
+                if(line.isBlank()) continue;
+                int i = Integer.parseInt(line);
+                if(lastNum == -1){
+                    lastNum = i;
+                    continue;
+                }
+
+                if (i > lastNum) {
+                    countIncrease++;
+                }
+                lastNum = i;
+
+            }
+            System.out.println(countIncrease);
+        } catch (IOException ex) {
+        throw new RuntimeException(ex);
+    }
+    }
+
 }
